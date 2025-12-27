863b5bd19ebe7a65303f5261e24d378986a1ab1d
manohar6323 <subbala.manohar@gmail.com>
2025-12-27 11:43:20 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter the number:");
+    scanf("%d",&number);
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+    return 0;
+}
