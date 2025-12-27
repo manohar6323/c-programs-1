4b5726d793a6f2416160087ceaefdbc4477c8fcf
manohar6323 <subbala.manohar@gmail.com>
2025-12-27 11:21:06 +0530

    Matrix Chain Multiplication.c

---
+++ Matrix Chain Multiplication.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+int mcm(int p[], int n) {
+    int dp[n][n];
+    for (int i = 1; i < n; i++)
+        dp[i][i] = 0;
+    for (int L = 2; L < n; L++) {
+        for (int i = 1; i < n-L+1; i++) {
+            int j = i + L - 1;
+            dp[i][j] = INT_MAX;
+            for (int k = i; k < j; k++) {
+                int q = dp[i][k] + dp[k+1][j]
+                        + p[i-1]*p[k]*p[j];
+                if (q < dp[i][j])
+                    dp[i][j] = q;
+            }
+        }
+    }
+    return dp[1][n-1];
+}
+int main() {
+    int arr[] = {1, 2, 3, 4};
+    int n = 4;
+    printf("Minimum multiplications = %d\n", mcm(arr, n));
+    return 0;
+}
